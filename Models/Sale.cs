a4371a0cfaedbe8cd9a644cd8d6deedfcaa9cc9e
Iago <iagobensen@gmail.com>
2023-01-29 17:42:03 -0300

    adiciona propriedade data

--- Models/Sale.cs
+++ Models/Sale.cs
@@ -8,9 +8,10 @@ namespace tech_test_payment_api.Models
     public class Sale
     {
         public int Id { get; set; }
-        public Seller? Seller { get; set; }
-        public List<Item>? Item { get; set; }
-        public Status? status { get; set; }
+        public Seller Seller { get; set; }
+        public List<Item> Item { get; set; }
+        public Status status { get; set; }
+        public DateTime date {get; set;}
 
     }
 }
