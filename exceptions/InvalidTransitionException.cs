22cf51a646bd179fb0eae8bfb38006471b4f2ed4
Iago <iagobensen@gmail.com>
2023-01-30 00:01:05 -0300

    alterações

--- exceptions/InvalidTransitionException.cs
+++ exceptions/InvalidTransitionException.cs
@@ -7,9 +7,16 @@ namespace tech_test_payment_api.exceptions
 {
     public class InvalidTransitionException : Exception
     {
+        private string _mensage; 
         public InvalidTransitionException(string erroMensage)
         {
+            _mensage = erroMensage;
+        }
 
+        override 
+        public string ToString()
+        {
+            return $"Transição inválida: {_mensage}";
         }
     }
 }
