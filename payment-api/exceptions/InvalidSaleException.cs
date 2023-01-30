b5ef6fcc761567ae187a822ca9d51762796997ef
Iago <iagobensen@gmail.com>
2023-01-30 02:51:39 -0300

    remove imports não usáveis

--- payment-api/exceptions/InvalidSaleException.cs
+++ payment-api/exceptions/InvalidSaleException.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace payment_api.exceptions
 {
     public class InvalidSaleException : Exception
